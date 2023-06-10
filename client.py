4b41232a3e7c0e81b570f30aae59fe4bca84ca79
anastasia <moiri@mail.ru>
2023-06-10 14:42:37 +0200

    Lesson 8 done

--- client.py
+++ client.py
@@ -6,34 +6,31 @@ import json
 import logging
 import Logs.config_client_log
 from decorators import log
+from threading import Thread
 
 #initialyze logger
 logger = logging.getLogger('client')
 
 
-presense_message = {
+
+@log(logger)
+def send_presence(mysocket, user_name):
+    presense_message = {
     "action": "presence",
     "time": time.mktime(datetime.now().timetuple()),
     "type": "status",
     "user": {
-        "account_name": "GigaChad",
+        "account_name": user_name,
         "status": "I'm still alive!"
     }
-}
-
-@log(logger)
-def send_presence(mysocket):
+   }
     presense_message['time'] = time.mktime(datetime.now().timetuple())
     msg = json.dumps(presense_message)
     mysocket.send(msg.encode('utf-8'))
-    data = mysocket.recv(1000000)
-    resp_msg_dict = json.loads(data.decode('utf-8'))
-    # print('Server responded to presence message with code: ', resp_msg_dict["response"])
-    logger.debug('Server responded to presence message with code: %s', resp_msg_dict["response"])
 
 
-def main(argv):
-    # Parse command line arguments for port and address
+def parse_cmd_args(argv):
+   # Parse command line arguments for port and address
     address = 'localhost'
     port = 7777
     opts, args = getopt.getopt(argv,"ha:p:",["address=","port="])
@@ -46,6 +43,36 @@ def main(argv):
       elif opt in ("-p", "--port"):
          port = arg
     logger.info('Client started with parameters -p %s -a %s', port, address)
+    return address, port
+
+@log(logger)
+def receive_message(mysocket):
+    data = mysocket.recv(1000)
+    logger.info('New data received from server %s',data)
+    return json.loads(data.decode('utf-8'))     
+
+
+def receive_handler(mysocket):
+   while True:
+      msg_dict = receive_message(mysocket)
+      print(f'{datetime.fromtimestamp(float(msg_dict["time"]))} {msg_dict["from"]}: {msg_dict["message"]}')
+
+@log(logger)
+def send_message(mysocket, username, recipient, message_text):
+    message = {
+      "action": "msg",
+      "encoding": "utf-8",
+   }
+    message['time'] = time.mktime(datetime.now().timetuple())
+    message['from'] = username
+    message['to'] = recipient
+    message['message'] = message_text
+    msg = json.dumps(message)
+    mysocket.send(msg.encode('utf-8'))
+
+def main(argv):
+    # Parse command line arguments for port and address
+    address, port = parse_cmd_args(argv)
     # Make and bind socket
     s = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
     try:
@@ -53,17 +80,20 @@ def main(argv):
     except:
       logger.error('Could not connect to server %s:%s', address, port)
       return   
-    msg_sent_flag = False
-    while True:  
-       if ((datetime.now().second % 5) == 0):         
-          if not msg_sent_flag:           
-            send_presence(s)
-            msg_sent_flag = True
-       else:
-          msg_sent_flag = False
-    s.close()
-
-
+    
+    username = input('Your name: ')
+    send_presence(s, username)
+    print(f'Hello, {username}! Type recipient name and you message. Type quit to quit the app')
+    receive_thread =  Thread(target=receive_handler, args=(s, ))
+    receive_thread.daemon = True
+    receive_thread.start()
+    while True:
+         recipient = input('Recipient name: ')
+         message = input('Your message: ')
+         if recipient == 'quit' or message == 'quit':
+            sys.exit()
+         send_message(s, username, recipient, message)
+       
 
 if __name__ == "__main__":
    main(sys.argv[1:])
